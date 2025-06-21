fc69653f5b6e1511a67ec8fe818193d738298f25
Katherin Muñoz Guzman <177049041+Kat2419@users.noreply.github.com>
2025-06-21 13:27:52 -0500

    second commit

--- prueba.js
+++ prueba.js
@@ -1,1 +1,12 @@
 console.log("hola")
+
+let numeroUno =10;
+let numeroDos = 20;
+
+if (numeroDos > numeroUno){
+    vondolr.log("condicion verdadera");
+}
+else{
+    console.log("condicion falsa");
+}
+
